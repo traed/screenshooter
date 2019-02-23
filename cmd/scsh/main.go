346fed60756813b73b82efea9ffbdbaba3c90e57
Mattias Forsman <forsman1991@gmail.com>
2019-02-23 19:20:10 +0100

    Updated --help

--- cmd/scsh/main.go
+++ cmd/scsh/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
 	"sync"
@@ -11,12 +12,26 @@ import (
 )
 
 func main() {
-	path := flag.String("p", "../uploads", "The place where screenshots are saved.")
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage:")
+		fmt.Fprintln(flag.CommandLine.Output(), "scsh [-d=<path>] <url> [<url>...]")
+		fmt.Fprintln(flag.CommandLine.Output(), "")
+		fmt.Fprintln(flag.CommandLine.Output(), "Options:")
+		flag.PrintDefaults()
+	}
+
+	path := flag.String("d", "../uploads", "Directory where screenshots are saved.")
 	flag.Parse()
 
 	var wg sync.WaitGroup
 	browser := chrome.NewChrome()
 	browser.SetScreenshotPath(*path)
+
+	urls := flag.Args()
+
+	if len(urls) == 0 {
+		log.Fatal("No URLs provided.")
+	}
 
 	for _, arg := range flag.Args() {
 		u, err := url.ParseRequestURI(arg)
